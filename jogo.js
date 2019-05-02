c47af1c18889094401c05aaadf355f551b8cf1fd
Paulo Germano <paulogermano.123pg@hotmail.com>
2019-05-02 15:05:55 -0300

    jogadores ativos

--- jogo.js
+++ jogo.js
@@ -1,6 +1,28 @@
+var rodada = 1;
+var matriz = Array(3);
 $(document).ready(function () {
     $('#iniciar_jogo').click(function () {
         $('#pagina_do_personagem').hide();
         $('#area_do_jogo').show();
     });
+    $('.campo').click(function () {
+        var id_campo = this.id;
+        jogo_play(id_campo);
+    });
+    function jogo_play(id) {
+        var simbolo = '';
+        var pontos = 0;
+
+        if ((rodada % 2) == 0){
+            simbolo = 'url(imagens/Ocangaço.png)';
+            pontos = 1;
+        }else {
+            simbolo = 'url(imagens/xpassaro.png)';
+            pontos = -1;
+        }
+
+        rodada++;
+
+        $('#'+id).css('background-image',simbolo);
+    }
 });
